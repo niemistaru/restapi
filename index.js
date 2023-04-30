5c0538b92d07b6bc245e7be50e3438c891f612e8
niemistaru <taru.nieminen@student.laurea.fi>
2023-04-30 22:02:36 +0300

    Pikafiksaus

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true }));
 //Yhteys tietokantaan
 const client = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-import './style.css'
+//import './style.css'
 
 /*****************REITIT***********************/
 //Serve a fom to the user
